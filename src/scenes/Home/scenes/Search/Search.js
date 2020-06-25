468f6eb40202ddbfc881b0ba2a6f18d3f45827ca
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-06-25 11:04:03 +0800

    add store

--- src/scenes/Home/scenes/Search/Search.js
+++ src/scenes/Home/scenes/Search/Search.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { toastr } from 'react-redux-toastr';
 import { bindActionCreators } from 'redux';
+import { toastr } from 'react-redux-toastr';
+import Swal from 'sweetalert2';
+import queryString from 'query-string';
+
+// Import actions
+import {
+getSearch
+} from '../../../../services/search/searchActions';
+
+// Import settings
+import settings from '../../../../config/settings';
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        // console.log(props)
+        this.state = {
+            city: '',
+            address: '',
+            area:''
+        };
+        this.change = this.change.bind(this);
+        this.handleGetResult = this.handleGetResult.bind(this);
+    } 
+    change(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleGetResult() {
+        const { city, address, area } = this.state;
+        console.log(city)
+    }
     render() {
         return (
             <div id="search">
@@ -14,20 +45,20 @@ class Search extends React.Component {
                             <div className="d-flex">
                                 <div className="input-container">
                                     <i className="fa fa-building icon"></i>
-                                    <input className="input-field city-input" type="text" placeholder="Enter City" name="city" required />
+                                    <input className="input-field city-input" type="text" placeholder="Enter City" name="city" onChange={this.change} />
                                 </div>
                             </div>
                             <div className="d-flex">
                                 <div className="input-container">
                                     <i className="fa fa-map-marker icon" aria-hidden="true"></i>
-                                    <input className="input-field city-input" type="text" placeholder="Enter Address" name="address" required />
+                                    <input className="input-field city-input" type="text" placeholder="Enter Address" name="address" onChange={this.change} />
                                 </div>
                                 <div className="input-container">
                                     <i className="fa fa-location-arrow icon"></i>
-                                    <input className="input-field city-input" type="text" placeholder="Enter Area" name="area" required />
+                                    <input className="input-field city-input" type="text" placeholder="Enter Area" name="area" onChange={this.change} />
                                 </div>
                             </div>
-                            <button type="submit" className="btn">Check availability</button>
+                            <button type="button" className="btn" onClick={this.handleGetResult}>Check availability</button>
                         </form>
                     </div>
                 </div>
@@ -36,4 +67,16 @@ class Search extends React.Component {
     }
 }
 
-export default Search;
+// export default Search;
+export default connect(
+    state => ({
+        ...state.default.services.category
+    }),
+    dispatch => ({
+        searchActions: bindActionCreators(
+            { getSearch },
+            dispatch
+        ),
+        
+    })
+)(Search);
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
-import createHistory from 'history/createBrowserHistory';
+
+import { createBrowserHistory } from 'history';
 import { reducer as formReducer } from 'redux-form';
 import { reducer as toastrReducer } from "react-redux-toastr";
 // Import reducers
@@ -17,7 +18,7 @@ searchSubscriber
 } from './services/saga';
 
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const initialState = {};
 const enhancers = [];
